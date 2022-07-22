8f19a5f7430ba23138d96411f648fdd4206a0b5a
jingkui.wang <jingkui.wang@imp.ac.at>
2022-07-22 14:25:22 +0200

    add barcode whitelist for mutliome

---
+++ assets/README.sh
@@ -0,0 +1,8 @@
+# the whitelist barcode is for scRNAseq and is found
+/software/2020/software/cellranger/7.0.0/lib/python/cellranger/barcodes/3M-february-2018.txt.gz
+
+# info from 10x web
+https://kb.10xgenomics.com/hc/en-us/articles/360031133451-Why-is-there-a-discrepancy-in-the-3M-february-2018-txt-barcode-whitelist-
+
+# 10 mutiome barcode is found
+/software/2020/software/cellranger-arc/2.0.0/lib/python/cellranger/barcodes/737K-arc-v1.txt.gz
